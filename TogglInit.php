fd643f388f711bdd12248c795a59d441d3397ecc
bantioco <benjamin.antioco@glasshouse.fr>
2019-02-27 07:42:25 +0100

    PHP - optimize

--- TogglInit.php
+++ TogglInit.php
@@ -9,14 +9,20 @@ class TogglInit
 	public function __construct()
 	{
 		require __DIR__.DIRECTORY_SEPARATOR.'config.php';
-
 		$this->apitoken = $TOGGL_TOKEN;
 		$this->baseurl 	= $TOGGL_BASE_URL;
 	}
 
 	public function initConnexion()
 	{
-		$options 	= $this->options();
+		$response = $this->curlConnexion( 'me', $this->options() );
+
+		return $this->initResult( $response );
+	}
+
+
+	public function curlConnexion( $routeUrl, $options )
+	{
 		$curl 		= curl_init();
 
 		if (isset($options['data'])) {
@@ -25,32 +31,42 @@ class TogglInit
 			$options['headers']['Content-Type'] = 'application/json';
 		}
 
-		curl_setopt($curl, CURLOPT_URL, "{$this->baseurl}/me");
+		curl_setopt($curl, CURLOPT_URL, $this->baseurl.DIRECTORY_SEPARATOR.$routeUrl);
 
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
 		curl_setopt($curl, CURLOPT_HEADER, FALSE);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // Needed since Toggl's SSL fails without this.
+		
+		//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+		curl_setopt($curl, CURLOPT_CAINFO, __DIR__.DIRECTORY_SEPARATOR.'toggl.crt');
+		
 		//curl_setopt($curl, CURLOPT_USERAGENT, $this->userAgent);
+
 		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $options['method']);
+
 		curl_setopt($curl, CURLOPT_USERPWD, "{$this->apitoken}:api_token");
 
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $options['headers']);
-		// Perform the API request.
+
 		$result = curl_exec($curl);
 
 		if ($result == FALSE) {
 			//throw new TogglException(curl_error($curl));
-
 			return false;
 		}
-		// Build the response.
+
 		$response = new stdClass();
+
 		$response->data = json_decode($result, TRUE);
+
 		$response->code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
 		$response->success = $response->code == 200;
+
 		curl_close($curl);
 
-		return $this->initResult( $response );
+		return $response;
 	}
 
 	public function initResult( $response )
